0e93b4f4e5aa6a406980eb9bf90bb134ce262e9e
Harkiran7237 <60743030+Harkiran7237@users.noreply.github.com>
2021-01-12 08:31:50 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,16 @@
+class Bob {
+    constructor(x,y){
+var options ={
+	
+	'restitution': 0.3,
+	'friction': 0.5,
+    'density': 1.2
+}
+this.body = Bodies.circle(x,y,21,options);
+World.add(world, this.body);
+}
+display(){
+	rectMode(RADIUS);
+	ellipse(this.body.position.x,this.body.position.y,21,21);
+	}
+};
